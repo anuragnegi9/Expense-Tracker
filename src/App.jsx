dc78b2ad9d5eae17b90d171c880478ada0af0772
anurag_negi9 <anruagnegi7455@gmail.com>
2026-07-30 23:04:18 +0530

    Fix Charts filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import ExpensesForm from "./components/ExpensesForm";
 import ExpensesList from "./components/ExpensesList";
 import Dashboard from "./components/Dashboard";
-import Charts from "./components/Charts";
+import Charts from "./components/charts";
 import Statistics from "./components/Statistics";
 
 function App() {
